2e8ee47f467e3297288de10fa4697b8cf2e15ec1
Philippe Leblond <leblond.philippe@gmail.com>
2015-08-28 13:51:18 -0400

    Basic files

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// CREATE HTTP SERVER AND PROXY
+
+var app = require('express')();
+
+app.use(require('morgan')('dev'));
+
+// LOAD CONFIGURATION
+
+app.set('port', process.env.PORT || 2000);
+
+// CONFIGURE /APP/* ROUTES
+
+app.use('/undb', require('serve-static')(__dirname + '/undb'));
+
+// app.use('/soi/app',   require('serve-static')(__dirname + '/app_build'));
+// app.use('/soi/app',   require('serve-static')(__dirname + '/app'));
+// app.all('/soi/app/*', function(req, res) { res.status(404).send(); } );
+
+// CONFIGURE TEMPLATE.HTML
+
+// app.get('/soi',   function (req, res) { res.sendfile(__dirname + '/app/template.html'); });
+// app.get('/soi/*', function (req, res) { res.sendfile(__dirname + '/app/template.html'); });
+
+// START SERVER
+
+app.listen(app.get('port'), function () {
+	console.log('Server listening on %j', this.address());
+});
