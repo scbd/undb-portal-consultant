775bb5e9229b536a9eabbf352f74ec402f874a3c
owkwen <faraj.ilyas@gmail.com>
2015-09-24 15:29:33 -0400

    Linkblock icons, map partial

---
+++ undb/partials/map.php
@@ -0,0 +1,41 @@
+<div id="map">
+	<div class="centerREMOVE">
+		<div class="sideSelection col-md-3">
+
+			<div class="floatRight">
+				<h5>UNDB Network</h5>
+				<ul>
+					<li class="active" data-text="The UNDB Network comprises all Actors contributing to the implementation of the 2011-2020 Strategic Plan for Biodiversity.">Actors</li>
+					<li data-text="With 196 <a href='un.org'>Parties</a>, the Convention on Biological Diversity has near universal participation among countries.">Parties to the CBD</li>
+					<li data-text="The Biodiversity Champions are truly inventive, creative and exceptional examples of support to the Strategic Plan for Biodiversity 2011-2020.">Biodiversity Champions</li>
+					<li data-text=" Case studies are narrative examples of great success stories. Find inspiration for your own Actions!">Case Studies</li>
+					<li data-text="Projects are long term initiatives at the national level or beyond, and feature elements of capacity building.">Specific Projects</li>
+					<li data-text="All around the world people are taking action to safeguard biodiversity. See how you can participate!">
+						Actions 
+						<select name="action_year" class="form-control">
+							<option value="">2011</option>
+							<option value="">2012</option>
+							<option value="">2014</option>
+							<option value="">2015</option>
+							<option value="">2016</option>
+							<option value="">2017</option>
+							<option value="">2018</option>
+							<option value="">2019</option>
+							<option value="">2020</option>
+						</select>
+					</li>
+				</ul>
+			</div>
+
+		</div>
+		<div id="googlemap" class="col-md-9">
+			<!-- Replace img with the Map  -->
+			<img src="/undb/img/map.png" alt="">
+			<div class="mapPopup">
+				<p>The UNDB Network comprises all Actors contributing to the implementation of the 2011-2020 Strategic Plan for Biodiversity.</p>
+				<button type="button" class="closeButton"></button>
+			</div>
+		</div>	
+
+	</div>		
+</div>
