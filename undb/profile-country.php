beba13241528257de633ef776578da9578753131
owkwen <faraj.ilyas@gmail.com>
2015-09-27 11:57:06 -0400

    profile pages (country, organization and event)

---
+++ undb/profile-country.php
@@ -0,0 +1,234 @@
+<?php include(__DIR__ . '/partials/header.php'); ?>
+
+
+
+
+<div class="center profile">
+		
+		<div class="flag">
+			<img src="" alt="">
+			<h1>Canada</h1>
+		</div>
+
+	  <ul class="nav nav-tabs" role="tablist">
+	    <li role="presentation" class="active"><a href="#UNDB" aria-controls="UNDB" role="tab" data-toggle="tab">UNDB</a></li>
+	    <li role="presentation"><a href="#CBD" aria-controls="CBD" role="tab" data-toggle="tab">CBD</a></li>
+	    <li role="presentation"><a href="#More" aria-controls="More" role="tab" data-toggle="tab">More</a></li>
+	  </ul>
+
+		<div class="profile-border">
+		<!-- DIV BORDER ( colum + center )  -->
+
+			<div class="profile-column">
+				<img src="/undb/img/country-map.png" alt="">
+
+				<ul class="party">
+					<li>			
+						<span>Convention on Biological Diversity</span>
+						<span>Party</span>
+					</li>	
+					<li>
+						<span>Cartagena Protocol</span>
+						<span>Non Party</span>
+					</li>	
+					<li>
+						<span>Cartagena Protocol</span>
+						<span>Non Party</span>
+					</li>	
+					<li>
+						<span>Nagoya Protocol</span>
+						<span>Non Party</span>
+					</li>	
+					<li>
+						<span>Nagoya–Kuala Lumpur Protocol</span>
+						<span>Non Party</span>						
+					</li>	
+
+				</ul>
+				
+
+				<ul class="links">
+					<li>
+							<a href="#">
+								<span class="www-icon">www</span>
+								<span>[website address]</span>
+							</a>
+					</li>
+					<li>
+							<a href="#">
+								<span class="at-icon">@</span>
+								<span>[contact@email]</span>
+							</a>
+					</li>
+					<li>
+							<a href="#">
+								<span><?php include(__DIR__ . '/img/icon/facebook.svg'); ?></span>
+								<span>[facebook.com/page]</span>
+							</a>
+					</li>
+					<li>
+							<a href="#">
+								<span><?php include(__DIR__ . '/img/icon/twitter.svg'); ?></span>
+								<span>[twitter.com/]</span>
+							</a>
+					</li>
+				</ul>
+			</div>
+
+		  <div class="tab-content">
+		    <div role="tabpanel" class="tab-pane active" id="UNDB">
+		    	<div class="article">
+			    	<p>
+							This field describes the actions taken by the Party toward implementing the Strategic Plan. The description can be inserted by the Focal Point, but can be edited by SCBD staff, also to allow inserting CMS codes/wiki syntax. It can be inserted in any of the 6 UN languages, max 600 characters. Massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu,
+			    	</p>
+			    	<p>
+			    		Description in official language (optional): local language translation of the description above, max 600 characters. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et  sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu,
+			    	</p>
+					</div>
+
+
+					<h4 class="darkGreen">Upcoming Activities</h4>
+					<table class="calendar-result">
+							<!-- Header -->		
+							<tr>
+								<th>Dates and Venue</th>
+								<th>Activity</th>
+							</tr>
+
+							<!-- Section -->
+							<tr class="section">
+								<td colspan="2">July 2015</td>
+							</tr>
+
+							<!-- Lines -->
+							<tr>
+								<td>
+									<span class="darkGreen">8 July 2015</span>
+									<span class="textGray">Paris, France</span>
+									<span class="darkGreen">Confirmed</span>
+								</td>
+								<td>
+									<p>Fifth meeting of the Liaison Group on the Global Strategy for Plant Conservation</p>
+									<a href="#">» Documents</a>
+								</td>
+							</tr>
+
+							<tr>
+								<td>
+									<span class="darkGreen">8 July 2015</span>
+									<span class="textGray">Paris, France</span>
+									<span class="darkGreen">Confirmed</span>
+								</td>
+								<td>
+									<p>Fifth meeting of the Liaison Group on the Global Strategy for Plant Conservation</p>
+									<a href="#">» Documents</a>
+								</td>
+							</tr>		
+						</table>	  
+
+						<div class="space">
+							<a href="" class="button green">More Activities</a>
+						</div>
+
+
+					<h4 class="darkGreen">UNDB Network</h4>
+					<div class="listImg">
+							<ul>
+								<li>
+									<div class="col-sm-3">
+										<img src="/undb/img/logo/FAO.png" alt="">
+									</div>
+									<div class="col-sm-9">
+										<h5>Canadian Biosphere Reserves Association</h5>
+										<a href="biospherecanada.ca/en">biospherecanada.ca/en</a>
+										<p>
+											Biosphere reserves in Canada are ecologically significant regions that promote sustainability and conservation by engaging all community stakeholders. Recognized by Unesco, they support reserch....
+										</p>
+									</div>
+								</li>
+								<li>
+									<div class="col-sm-3">
+										<img src="/undb/img/logo/FAO.png" alt="">
+									</div>
+									<div class="col-sm-9">
+										<h5>International Development Law Organization</h5>
+										<a href="biospherecanada.ca/en">biospherecanada.ca/en</a>
+										<p>
+											IDLO is the only intergovernmental organization with an exclusive mandate to promote the rule of law. Governments, multilateral organizations, private foundations and the private sector support our work  <a href="">></a>
+										</p>
+									</div>
+								</li>
+							</ul>
+						</div>
+
+						<div class="space">
+							<a href="" class="button green">More</a>
+						</div>
+
+				<!-- END FIRST TAB DIV -->
+		    </div>
+		    <div role="tabpanel" class="tab-pane" id="CBD">
+
+		    	<div class="article">
+		    		<h3>Convention on Biological Diversity - Country Profile</h3>
+			    	<p>
+							Please note that this country profile has been prepared by the Secretariat on the basis of information provided in the NBSAP and national reports, but has not been reviewed by the Party concerned. <a href="#">CBD Country Profiles homepage ></a>
+			    	</p>
+		    		<h3>Access and Benefit-Sharing Clearing-House</h3>
+			    	<p>
+							The Access and Benefit-sharing Clearing-house (ABSCH) is a platform for exchanging information on ABS and a key tool for facilitating the implementation of the Nagoya Protocol.  <a href="#">ABSCH homepage ></a>
+			    	</p>
+		    		<h3>Biosafety Clearing-House</h3>
+			    	<p>
+							The Biosafety Clearing-House (BCH) is a mechanism set up by the Cartagena Protocol on Biosafety to facilitate the exchange of information on Living Modified Organisms (LMOs) and assist the Parties to better comply with their obligations under the Protocol. Global access to a variety of scientific, technical, environmental, legal and capacity building information is provided in the six official languages of the UN. <a href="#">BCH homepage ></a>
+			    	</p>
+		    		<h3>National Clearing-House Mechanism</h3>
+			    	<p>
+							The Clearing-House Mechanism (CHM) of the Convention on Biological Diversity has been established further to Article 18.3 of the Convention.
+							Further to decision X/15, its mission is to contribute significantly to the implementation of the Convention on Biological Diversity and its Strategic Plan for Biodiversity 2011-2020, through effective information services and other appropriate means in order to promote and facilitate scientific and technical cooperation, knowledge sharing and information exchange, and to establish a fully operational network of Parties and partners. <a href="#">CHM homepage ></a>
+			    	</p>		    			    	
+					</div>	    	
+
+		    <!-- END SECOND TAB DIV -->	
+		    </div>
+		    <div role="tabpanel" class="tab-pane" id="More">
+		    	
+		    	<div class="article narrow">
+		    		<h3>Environment Canada</h3>
+			    	<p>
+							Please note that this country profile has been prepared by the Secretariat on the basis of information provided in the NBSAP and national reports, but has not been reviewed by the Party concerned. <a href="#">CBD Country Profiles homepage ></a>
+			    	</p>
+		    		<h3>InforMEA</h3>
+			    	<p>
+							InforMEA is the first project established by this Initiative. InforMEA harvests COP decisions and resolutions, news, events, MEA membership, national focal points, national reports and implementation plans from MEA secretariats and organizes this information around a set of agreed terms. InforMEA country profiles >
+			    	</p>
+		    		<h3>UNdata</h3>
+			    	<p>
+							UNdata is an internet-based data service which brings UN statistical databases within easy reach of users through a single entry point. Users can now search and download a variety of statistical resources of the UN system. <a href="#">UNdata country profiles ></a>
+			    	</p>
+		    		<h3>ProtectedPlanet.net</h3>
+			    	<p>
+							ProtectedPlanet.net is the online interface for the World Database on Protected Areas (WDPA), a join project of IUCN and UNEP, and the most comprehensive global database on terrestrial and marine protected areas. ProtectedPlanet.net lets you discover the protected areas of the world through exploring the maps and intuitive searching, feeding you information from the WDPA, photos from Panoramio and text descriptions from Wikipedia. <a href="#">ProtectedPlanet homepage ></a>
+			    	</p>		    
+			    	<h3>UNESCO World Heritage List</h3>
+			    	<p>
+			    		The United Nations Educational, Scientific and Cultural Organization (UNESCO) seeks to encourage the identification, protection and preservation of cultural and natural heritage around the world considered to be of outstanding value to humanity. This is embodied in an international treaty called the Convention concerning the Protection of the World Cultural and Natural Heritage , adopted by UNESCO in 1972. <a href="#">World Heritage List ></a>
+			    	</p>
+					</div>
+
+		    <!-- END THIRD TAB DIV -->	
+		    </div>
+		  </div>
+
+<!-- END DIV BORDER ( colum + center ) -->
+</div>
+
+
+
+<!-- END DIV CENTER -->
+</div>
+
+
+
+
+<?php include(__DIR__ . '/partials/footer.php'); ?>
