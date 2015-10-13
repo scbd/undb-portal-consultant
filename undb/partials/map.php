dcbb61de10c624ccb45a5a86a4163074aaf089bd
owkwen <faraj.ilyas@gmail.com>
2015-10-13 05:32:53 -0400

    map default and 'All' value

--- undb/partials/map.php
+++ undb/partials/map.php
@@ -17,12 +17,13 @@
 							<option value="">2011</option>
 							<option value="">2012</option>
 							<option value="">2014</option>
-							<option value="">2015</option>
+							<option value="" selected>2015</option>
 							<option value="">2016</option>
 							<option value="">2017</option>
 							<option value="">2018</option>
 							<option value="">2019</option>
 							<option value="">2020</option>
+							<option value="">All</option>
 						</select>
 					</li>
 				</ul>
